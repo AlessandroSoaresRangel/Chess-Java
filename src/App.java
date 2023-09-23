8dd864455486267826bc1a2afe61629f729abd07
Alessandro <alessandrosoaresrangel1@gmail.com>
2023-09-23 16:12:32 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,35 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import chess.ChessException;
+import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        ChessMatch chessMatch = new ChessMatch();
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            try {
+                UI.ClearConsole();
+                UI.printBoard(chessMatch.getPieces());
+                System.out.println();
+                System.out.print("Source: ");
+                ChessPosition source = UI.readChessPosition(sc);
+                System.out.println();
+                System.out.println("Target: ");
+                ChessPosition target = UI.readChessPosition(sc);
+
+                ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+            } catch (ChessException e) {
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            } catch (InputMismatchException e) {
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
+
+        }
+    }
+}
